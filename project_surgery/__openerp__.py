272371f28369cca514d90f355e7771c133d11dcf
gwsilva <gideoni.silva@omnes.net.br>
2014-08-25 16:04:49 -0300

    Create __openerp__.py

---
+++ project_surgery/__openerp__.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author: Gideoni Silva (Omnes)
+#    Copyright 2013-2014 Omnes Tecnologia
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Participantes da Cirurgia',
+    'description': 'Este módulo adiciona os campos adicionais com os participantes da cirurgia.',
+    'category': 'Generic Modules/Projects & Services',
+    'license': 'AGPL-3',
+    'author': 'Omnes',
+    'website': 'www.omnes.net.br',
+    'version': '0.1',
+    'depends': [
+        'base',
+        'project',
+    ],
+    'data': [
+        'project_view.xml'
+    ],
+    'demo': [],
+    'installable': True,
+    'active': False,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
